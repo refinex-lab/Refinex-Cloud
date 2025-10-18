8876daed5318a9d0f195d1d8dfc953925f1b0357
refinex <refinex@163.com>
2025-10-18 15:21:26 +0800

    feat：logo 替换

--- refinex-ui/config/defaultSettings.ts
+++ refinex-ui/config/defaultSettings.ts
@@ -1,24 +1,39 @@
-import type { ProLayoutProps } from '@ant-design/pro-components';
+import type {ProLayoutProps} from '@ant-design/pro-components';
 
 /**
- * @name
+ * 全局布局配置
  */
 const Settings: ProLayoutProps & {
   pwa?: boolean;
   logo?: string;
 } = {
+  // 导航主题
   navTheme: 'light',
-  // 拂晓蓝
+  // 主色调
   colorPrimary: '#1890ff',
+  // 布局模式
   layout: 'mix',
+  // 内容宽度
   contentWidth: 'Fluid',
+  // 是否固定 Header
   fixedHeader: false,
+  // 是否固定 Siderbar
   fixSiderbar: true,
+  // 是否开启弱色模式
   colorWeak: false,
-  title: 'Ant Design Pro',
+  // 标题
+  title: 'Refinex Hub',
+  // 是否拆分菜单
+  splitMenus: false,
+  // Siderbar 菜单类型
+  siderMenuType: "sub",
+  // 是否开启 PWA
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  // logo 图片地址
+  logo: '/logo.svg',
+  // iconfont 字体地址
   iconfontUrl: '',
+  // token 配置
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
     //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
