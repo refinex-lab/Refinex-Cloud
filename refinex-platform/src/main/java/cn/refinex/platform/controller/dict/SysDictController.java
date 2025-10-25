a57696593b72a8dac5073d4265c5761dc3464803
refinex <refinex@163.com>
2025-10-25 21:17:04 +0800

    fix: MySQL 分页计算修复

--- refinex-platform/src/main/java/cn/refinex/platform/controller/dict/SysDictController.java
+++ refinex-platform/src/main/java/cn/refinex/platform/controller/dict/SysDictController.java
@@ -28,7 +28,7 @@ import java.util.List;
  * @since 1.0.0
  */
 @RestController
-@RequestMapping("/system/dictionaries")
+@RequestMapping("/dicts")
 @RequiredArgsConstructor
 @Tag(name = "系统字典管理", description = "字典类型和字典数据的增删改查等接口")
 public class SysDictController {
